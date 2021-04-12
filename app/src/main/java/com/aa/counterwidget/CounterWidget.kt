1ab26e9f0c529dfbba59bd9a8a0f9c749eb32800
alexalbert <albalex@gmail.com>
2021-04-12 14:49:56 -0400

    First cut, mostly working

--- app/src/main/java/com/aa/counterwidget/CounterWidget.kt
+++ app/src/main/java/com/aa/counterwidget/CounterWidget.kt
@@ -1,14 +1,29 @@
 package com.aa.counterwidget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
+
+var REFRESH_ACTION = "android.appwidget.action.APPWIDGET_UPDATE"
+val CLICK_ACTION = "android.appwidget.action.CLICK"
 
 /**
  * Implementation of App Widget functionality.
  */
 class CounterWidget : AppWidgetProvider() {
+
+    companion object {
+        private var count = 0
+    }
+
+    private var views: RemoteViews? = null
+
+
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -16,7 +31,7 @@ class CounterWidget : AppWidgetProvider() {
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            updateAppWidget(context, appWidgetManager, appWidgetId, 0)
         }
     }
 
@@ -27,17 +42,40 @@ class CounterWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        val views = RemoteViews(context!!.packageName, R.layout.counter_widget)
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        val appWidgetIds = appWidgetManager.getAppWidgetIds(ComponentName(context, CounterWidget::class.java))
+
+        if (intent!!.action == CLICK_ACTION) {
+            count++
+        } else { // Resize
+            count = 0
+        }
+
+        views.setTextViewText(R.id.counter_button, count.toString())
+        appWidgetManager.updateAppWidget(appWidgetIds, views)
+    }
 }
 
 internal fun updateAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
+    appWidgetId: Int,
+    count: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.counter_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    views.setTextViewText(R.id.counter_button, count.toString())
+
+    val intent = Intent(context, CounterWidget::class.java)
+    intent.action = CLICK_ACTION
+    val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+
+    views.setOnClickPendingIntent(R.id.counter_button, pendingIntent)
+
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
